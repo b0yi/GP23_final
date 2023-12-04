c75a00e3055ef0fc6168c96432eb3e450dd98168
b0yi <jsmdjsmdjsmdo@gmail.com>
2023-12-04 22:01:53 +0800

    add some sprite to cat planet and add monster and post processing in blackhole

--- Assets/Scripts/Neutral Enemy/RotateToTarget.cs
+++ Assets/Scripts/Neutral Enemy/RotateToTarget.cs
@@ -8,17 +8,20 @@ public class RotateToTarget : MonoBehaviour
     
     public float rotationSpeed;
     private Vector2 direction;
+    public GameObject target;
 
 
     private void Update()
     {
-        direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        direction = target.transform.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
 
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 cursorPos = target.transform.position;
         transform.position = Vector2.MoveTowards(transform.position, cursorPos, moveSpeed * Time.deltaTime);
 
     }
-}
+}               
