bda0ade68d4889b164e6b118f4ad0bae0be3ad2e
enfu.liao.cs10@nycu.edu.tw <enfu.liao.cs10@nycu.edu.tw>
2023-12-06 14:37:44 +0800

    Planet preview (PlanetE, PlanetF)
    - wait for works about PlanetE & PlanetF

--- Assets/Scripts/Triange/PreviewPlanet.cs
+++ Assets/Scripts/Triange/PreviewPlanet.cs
@@ -4,26 +4,60 @@ using UnityEngine;
 using UnityEngine.Playables;
 public class PreviewPlanet : MonoBehaviour
 {
-    public PlayableDirector timeline;
+    public PlayableDirector timelinePlanetR;
+    public PlayableDirector timelinePlanetE;
+    public PlayableDirector timelinePlanetF;
 
     public PlayerController player;
+    [DisplayOnly] public UIManager uIManager;
 
     // 之後根據不同 player 的狀態（現在要去哪個星球來播放不同的 timeline）
+
+    void Start()
+    {
+        uIManager = GameObject.FindWithTag("UIManager").GetComponent<UIManager>();
+    }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.CompareTag("Player"))
         {
-            timeline.Play();
-            player.isLocked = true;
+
+            if (uIManager.stage == GameProgressStage.PlanetR)
+            {
+                timelinePlanetR.Play();
+                player.isLocked = true;
+            }
+            if (uIManager.stage == GameProgressStage.PlanetE)
+            {
+                timelinePlanetE.Play();
+                player.isLocked = true;
+            }
+            if (uIManager.stage == GameProgressStage.PlanetF)
+            {
+                timelinePlanetF.Play();
+                player.isLocked = true;
+            }
+
+
+
         }
     }
 
     void Update()
     {
-        if (timeline.state == PlayState.Paused)
+        if (uIManager.stage == GameProgressStage.PlanetR && timelinePlanetR.state == PlayState.Paused)
         {
             player.isLocked = false;
         }
+        if (uIManager.stage == GameProgressStage.PlanetE && timelinePlanetE.state == PlayState.Paused)
+        {
+            player.isLocked = false;
+        }
+        if (uIManager.stage == GameProgressStage.PlanetF && timelinePlanetF.state == PlayState.Paused)
+        {
+            player.isLocked = false;
+        }
+
     }
 }
