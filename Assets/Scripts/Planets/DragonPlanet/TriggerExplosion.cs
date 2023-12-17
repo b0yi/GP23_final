5bdff3fc7b236287ed547fd339d4c841cb78d59c
b0yi <jsmdjsmdjsmdo@gmail.com>
2023-12-17 16:52:32 +0800

    Complete function of dragon planet
    rt (by lnfu)

---
+++ Assets/Scripts/Planets/DragonPlanet/TriggerExplosion.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
+
+//[RequireComponent(typeof(Explodable))]
+public class TriggerExplosion : MonoBehaviour 
+{
+
+	public Explodable explodable;
+    public PlayerController_new playerController;
+    [DisplayOnly] public float delayExplosionTimer;
+    [DisplayOnly] bool explode;
+    public float delayExplosionTime;
+
+   
+
+    private void Start()
+    {
+        explode = false;
+    }
+
+    private void Update()
+    {
+        if (explode)
+        {
+            delayExplosionTimer -= Time.deltaTime;
+            
+            if (delayExplosionTimer <= 0)
+            {
+                explodable.explode();
+                playerController.Unlock();
+                Destroy(gameObject);
+            }
+
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !explode) 
+        {
+            explode = true;
+            delayExplosionTimer = delayExplosionTime;
+            playerController.Lock();
+            playerController.Transform();
+        }
+    }
+}
