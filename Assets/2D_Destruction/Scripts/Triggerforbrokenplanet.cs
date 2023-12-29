3c7c7fb55965d6b759b1df3f8936964202881df8
Maskerkonev <73166480+Maskerkonev@users.noreply.github.com>
2023-12-29 17:22:34 +0800

    broken planet

---
+++ Assets/2D_Destruction/Scripts/Triggerforbrokenplanet.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggerforbrokenplanet : MonoBehaviour
+{
+    public Explodable explodable;
+    //public PlayerController_new playerController;
+    [DisplayOnly] public float delayExplosionTimer;
+    [DisplayOnly] bool explode;
+    public float delayExplosionTime;
+    //public GameObject dragon;
+
+
+    private void Start()
+    {
+        explode = false;
+        //dragon.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (explode)
+        {
+            delayExplosionTimer -= Time.deltaTime;
+
+            if (delayExplosionTimer <= 0)
+            {
+                explodable.explode();
+                //playerController.Unlock();
+                Destroy(gameObject);
+                //dragon.SetActive(true);
+            }
+
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !explode)
+        {
+            explode = true;
+            delayExplosionTimer = delayExplosionTime;
+        }
+    }
+}
